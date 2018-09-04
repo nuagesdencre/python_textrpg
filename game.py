5907ad8901822ed0521ac909801dd28f051305db
Unknown <vero@nuagesdencre.com>
2018-09-04 12:19:44 +0100

    title screen and title screen selections

--- game.py
+++ game.py
@@ -19,5 +19,49 @@ class player:
         self.mp = 0
         self.status_effects = []
 
+
 myPlayer = player()
 
+
+#### Title Screen ####
+
+def title_screen_selections():
+    option = input("> ")
+    if option.lower() == ("play"):
+        start_game()  # placeholder until written
+    elif option.lower() == ("help"):
+        help.menu()
+    elif option.lower() == ("quit"):
+        sys.exit()
+    while option.lower() not in ['play', 'help', 'quit']:
+        print("Please enter a valid command.")
+        option = ("> ")
+        if option.lower() == ("play"):
+            start_game()  # placeholder until written
+        elif option.lower() == ("help"):
+            help.menu()
+        elif option.lower() == ("quit"):
+            sys.exit()
+
+
+def title_screen():
+    os.system('clear')
+    print('###############################################')
+    print('#          Welcome to this Text RPG           #')
+    print('###############################################')
+    print(' > Play                                        ')
+    print(' > Help                                        ')
+    print(' > Quit                                        ')
+    print(' Have fun.                                     ')
+    title_screen_selections()
+
+
+def help_menu():
+    print('###############################################')
+    print('#          Welcome to this Text RPG           #')
+    print('###############################################')
+    print('- Use up, down, left and right to move.        ')
+    print('- Type your commands                           ')
+    print('- Use "look" to inspect something              ')
+    print('- Email me if there is a bug.                  ')
+    title_screen_selections()
