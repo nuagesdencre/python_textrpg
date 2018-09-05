314c65c056ea8f5bc54ed26aef61c47c61c57068
Unknown <vero@nuagesdencre.com>
2018-09-05 10:50:23 +0100

    game code refactoring (wip)

--- game.py
+++ game.py
@@ -1,52 +1,54 @@
 # Python Text RPG
 # Created by V Savard, inspired by Bryan Tong's Tutorial
 
-import cmd
-import textwrap
+#import cmd
+#import textwrap
 import sys
 import os
 import time
-import random
+#import random
 
 screen_width = 100
 
 
 #### Player Setup ####
-class player:
-    def __init___(self):
+class Player:
+    def __init__(self):
         self.name = ''
         self.hp = 0
-        self.mp = 0
+        self.charisma = 0
+        self.job = ''
         self.status_effects = []
-        self.location = 'starting_zone'
+        self.location = 'start'
+        self.game_over = False
 
 
-myPlayer = player()
+myPlayer = Player()
 
 
 #### Title Screen ####
 
 def title_screen_selections():
     option = input("> ")
-    if option.lower() == ("play"):
-        start_game()  # placeholder until written
-    elif option.lower() == ("help"):
-        help.menu()
-    elif option.lower() == ("quit"):
+    if option.lower() == "play":
+        setup_game()
+    elif option.lower() == "help":
+        help_menu()
+    elif option.lower() == "quit":
         sys.exit()
     while option.lower() not in ['play', 'help', 'quit']:
-        print("Please enter a valid command.")
-        option = ("> ")
-        if option.lower() == ("play"):
-            start_game()  # placeholder until written
-        elif option.lower() == ("help"):
-            help.menu()
-        elif option.lower() == ("quit"):
+        print("Please enter a valid command (-play -help -quit).")
+        option = input("> ")
+        if option.lower() == "play":
+            setup_game()
+        elif option.lower() == "help":
+            help_menu()
+        elif option.lower() == "quit":
             sys.exit()
 
 
 def title_screen():
-    os.system('clear')
+    os.system('cls')
     print('###############################################')
     print('#         Welcome to this Text RPG!           #')
     print('###############################################')
@@ -100,8 +102,8 @@ solved_places = {'a1': False, 'a2': False, 'a3': False, 'a4': False,
 
 zonemap = {
     'a1': {
-        'ZONENAME': 'A1',
-        'DESCRIPTION': 'description',
+        'ZONENAME': 'start',
+        'DESCRIPTION': "This is the town's entrance",
         'EXAMINATION': 'examination',
         'SOLVED': False,
         'UP': '',
@@ -270,7 +272,7 @@ zonemap = {
 def print_location():
     print('\n' + ('#' * (4 + (len(myPlayer.location)))))
     print('# ' + myPlayer.location + ' #')
-    print('# ' + zonemap[myPlayer.position]['DESCRIPTION'] + ' #')
+    print('# ' + zonemap[myPlayer.location]['DESCRIPTION'] + ' #')
     print('\n' + ('#' * (4 + (len(myPlayer.location)))))
 
 
@@ -292,15 +294,21 @@ def prompt():
         player_examine(action.lower())
 
 
-def player_move(myAction):
+def player_move(action):
     ask = 'Where do you want to go exactly?\n'
     dest = input(ask)
     if dest in ['up', 'north']:
         destination = zonemap[myPlayer.location]['UP']
         mouvement_handler(destination)
     elif dest in ['left', 'west']:
+        destination = zonemap[myPlayer.location]['LEFT']
+        mouvement_handler(destination)
     elif dest in ['right', 'east']:
+        destination = zonemap[myPlayer.location]['RIGHT']
+        mouvement_handler(destination)
     elif dest in ['down', 'south']:
+        destination = zonemap[myPlayer.location]['DOWN']
+        mouvement_handler(destination)
 
 
 def mouvement_handler(destination):
@@ -310,13 +318,98 @@ def mouvement_handler(destination):
 
 
 def player_examine(action):
-    if zonemap[myPlayer.location]['SOLVED']
+    if zonemap[myPlayer.location]['SOLVED']:
         print('You have already been to this zone.')
     else:
         print('You can trigger a puzzle here.')
 
-    #### Game functionality ####
+
+#### Game functionality ####
+
+def main_game_loop():
+    while myPlayer.game_over is False:
+        prompt()
+    # here handle if puzzle have been solved (full completion)
 
 
-def start_game():
-    return
+def setup_game():
+    os.system('cls')
+
+    question1 = 'Hello, what is your name?\n'
+    for character in question1:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    player_name = input("> ")
+    myPlayer.name = player_name
+
+    question2 = 'What is your title/job?\n'
+    question2added = 'You can be a cook, a barman or barmaid, or a singer.\n'
+    for character in question2:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    for character in question2added:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.02)
+    player_job = input("> ")
+    valid_jobs = ['cook', 'barman', 'barmaid', 'singer']
+    while player_job.lower() not in valid_jobs:
+        player_job = input("> ")
+    if player_job.lower() in valid_jobs:
+        myPlayer.job = player_job
+        print('Good. We need a decent ' + player_job + ' in our pub.\n')
+
+    ### Player Stats ###
+    if myPlayer.job is 'cook':
+        self.hp = 100
+        self.charisma = 40
+    if myPlayer.job is 'barman':
+        self.hp = 70
+        self.charisma = 70
+    if myPlayer.job is 'barmaid':
+        self.hp = 60
+        self.charisma = 80
+    if myPlayer.job is 'singer':
+        self.hp = 45
+        self.charisma = 95
+
+    ### Introduction ###
+    question3 = 'Welcome ' + player_name + ' the ' + player_job + '!\n'
+    for character in question3:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    player_name = input("> ")
+    myPlayer.name = player_name
+
+    os.system('cls')
+    speech1 = 'Welcome to our humble pub.\n'
+    speech2 = 'We know you are new in this area.\n'
+    speech3 = 'Make a name for your self here and beyond!\n'
+    speech4 = 'Good luck, you will need it.\n'
+    for character in speech1:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    for character in speech2:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    for character in speech3:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    for character in speech4:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.17)
+    os.system('cls')
+    print('####################################')
+    print('##   The adventure starts now!    ##')
+    print('####################################')
+    main_game_loop()
+
+
+title_screen()
